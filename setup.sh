361cebe5a7a4452bb3dade04c186876a7dacffc0
/home/Snowyy <104605523+Snowyy777@users.noreply.github.com>
2026-06-21 15:18:12 -0300

    Atualizar o setup.sh

--- setup.sh
+++ setup.sh
@@ -4,13 +4,13 @@ echo "Iniciando..."
 
 pkg update
 pkg install pulseaudio -y
-pkg install x11-repo
-pkg install termux-x11-nightly
-pkg install proot-distro
+pkg install x11-repo -y
+pkg install termux-x11-nightly -y
+pkg install proot-distro -y
 proot-distro install debian
 proot-distro login debian -- bash -c "apt update -y
-apt install adduser nano
-apt install sudo
+apt install adduser nano -y
+apt install sudo -y
 adduser Vulca
 nano /etc/sudoers
 sudo apt install xfce4 -y"
